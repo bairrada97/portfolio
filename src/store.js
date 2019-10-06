e23b244f7dc31d03303417617ff4c71f19d7a6cc
Joao bairrada <bairrada1997@gmail.com>
2019-10-06 21:38:52 +0100

    UPD: resume link

--- src/store.js
+++ src/store.js
@@ -9,10 +9,11 @@ export default new Vuex.Store({
     projects: [
       {
         id: 0,
-        name: 'Chess Game',
-        labels: ['Html', 'Sass', 'Javascript', 'ES6 Classes'],
-        image: '/../assets/images/chess-01.png',
-        imagesSlider: [{
+        name: "Chess Game",
+        labels: ["Html", "Sass", "Javascript", "ES6 Classes"],
+        image: "/../assets/images/chess-01.png",
+        imagesSlider: [
+          {
             image: "/../assets/images/chess-01.png",
             description: "init board chess"
           },
@@ -24,18 +25,19 @@ export default new Vuex.Store({
             image: "/../assets/images/chess-03.png",
             description: "pieces in middle of board with queen selected"
           }
-
         ],
-        description: "Chess Game, is a simple game  when user's can play chess. It was also a project where I could pratice Javascript as a object oriented programming language.",
-        liveSite: 'https://bairrada97.github.io/Chess-Game/',
-        liveCode: 'https://github.com/bairrada97/Chess-Game',
+        description:
+          "Chess Game, is a simple game  when user's can play chess. It was also a project where I could pratice Javascript as a object oriented programming language.",
+        liveSite: "https://bairrada97.github.io/Chess-Game/",
+        liveCode: "https://github.com/bairrada97/Chess-Game"
       },
       {
         id: 1,
-        name: 'The Weather Forecast',
-        labels: ['Html', 'Sass', 'Jquery', 'PWA', "API"],
-        image: '/../assets/images/weather-app-06.png',
-        imagesSlider: [{
+        name: "The Weather Forecast",
+        labels: ["Html", "Sass", "Jquery", "PWA", "API"],
+        image: "/../assets/images/weather-app-06.png",
+        imagesSlider: [
+          {
             image: "/../assets/images/weather-app-01.png",
             description: "one screen on Daily tab"
           },
@@ -45,7 +47,8 @@ export default new Vuex.Store({
           },
           {
             image: "/../assets/images/weather-app-03.png",
-            description: "three screens, show different temperature on daily tab"
+            description:
+              "three screens, show different temperature on daily tab"
           },
           {
             image: "/../assets/images/weather-app-04.png",
@@ -56,16 +59,18 @@ export default new Vuex.Store({
             description: "app fit in mobile screens"
           }
         ],
-        description: "The Weather Forecast, It's a progressive web app when the user is able to know the current temperature, wind, humidty and precipitation in any location, as well as the next 5 days.",
-        liveSite: 'https://the-weather-forecast.firebaseapp.com/',
-        liveCode: 'https://github.com/bairrada97/WeatherApp',
+        description:
+          "The Weather Forecast, It's a progressive web app when the user is able to know the current temperature, wind, humidty and precipitation in any location, as well as the next 5 days.",
+        liveSite: "https://the-weather-forecast.firebaseapp.com/",
+        liveCode: "https://github.com/bairrada97/WeatherApp"
       },
       {
         id: 2,
-        name: 'Dyslexia App',
-        labels: ['Html', 'Sass', 'Jquery', 'PWA'],
-        image: '/../assets/images/dislexia-app-04.png',
-        imagesSlider: [{
+        name: "Dyslexia App",
+        labels: ["Html", "Sass", "Jquery", "PWA"],
+        image: "/../assets/images/dislexia-app-04.png",
+        imagesSlider: [
+          {
             image: "/../assets/images/dislexia-app-01.png",
             description: "main screen"
           },
@@ -81,126 +86,125 @@ export default new Vuex.Store({
             image: "/../assets/images/dislexia-app-04.png",
             description: "screen to choose how you feel"
           }
-
         ],
-        description: "Dyslexia App, It's an app to complement a goose game, to help people who don't suffer from dyslexia understand how life can be hard for people who suffer. This app It's built in standard Front-End skills, also was my first project in which I created a Progressive web app.",
-        liveSite: 'https://dyslexia-app.firebaseapp.com/',
-        liveCode: 'https://github.com/bairrada97/DyslexiaApp',
-      },
+        description:
+          "Dyslexia App, It's an app to complement a goose game, to help people who don't suffer from dyslexia understand how life can be hard for people who suffer. This app It's built in standard Front-End skills, also was my first project in which I created a Progressive web app.",
+        liveSite: "https://dyslexia-app.firebaseapp.com/",
+        liveCode: "https://github.com/bairrada97/DyslexiaApp"
+      }
     ],
-    interests: [{
+    interests: [
+      {
         id: 0,
-        image: '/../assets/images/d-luis-bridge.webp',
+        image: "/../assets/images/d-luis-bridge.webp",
         description: "D.Luis Bridge",
-        isSelected: false,
+        isSelected: false
       },
       {
         id: 1,
-        image: '/../assets/images/temple-of-debod.webp',
+        image: "/../assets/images/temple-of-debod.webp",
         description: "Temple of Debod",
-        isSelected: false,
+        isSelected: false
       },
       {
         id: 2,
-        image: '/../assets/images/budapest-parlament.webp',
+        image: "/../assets/images/budapest-parlament.webp",
         description: "Budapest Parlament",
-        isSelected: false,
+        isSelected: false
       },
       {
         id: 3,
-        image: '/../assets/images/leaf.webp',
+        image: "/../assets/images/leaf.webp",
         description: "Leaf",
-        isSelected: false,
+        isSelected: false
       },
       {
         id: 4,
-        image: '/../assets/images/aveiro.webp',
+        image: "/../assets/images/aveiro.webp",
         description: "Aveiro",
-        isSelected: false,
+        isSelected: false
       },
       {
         id: 5,
-        image: '/../assets/images/porto-landscape.webp',
+        image: "/../assets/images/porto-landscape.webp",
         description: "Porto Landscape",
-        isSelected: false,
+        isSelected: false
       },
       {
         id: 6,
-        image: '/../assets/images/andorra-landscape.webp',
+        image: "/../assets/images/andorra-landscape.webp",
         description: "Andorra Landscape",
-        isSelected: false,
-      },
+        isSelected: false
+      }
     ],
-    myImage: '/../assets/images/bairradaFace.png',
-    illustrationImage: '/../assets/images/illustrationImage.png',
+    myImage: "/../assets/images/bairradaFace.png",
+    illustrationImage: "/../assets/images/illustrationImage.png",
     illustrationDescription: "Illustration of me",
     socialIcons: {
       github: {
-        icon: '/../assets/images/Github.svg',
+        icon: "/../assets/images/Github.svg",
         name: "GitHub",
         link: "https://github.com/bairrada97"
       },
       linkedin: {
-        icon: '/../assets/images/LinkedIn.svg',
+        icon: "/../assets/images/LinkedIn.svg",
         name: "Linkedin",
         link: "https://www.linkedin.com/in/joao-bairrada/"
       },
       instagram: {
-        icon: '/../assets/images/Instagram.svg',
+        icon: "/../assets/images/Instagram.svg",
         name: "Instagram",
         link: "https://www.instagram.com/_bairrada_/"
       },
       cv: {
-        icon: '/../assets/images/iOSPaper.svg',
+        icon: "/../assets/images/iOSPaper.svg",
         name: "CV",
-        link: "/../assets/images/bairradaResume.pdf"
+        link: "/../assets/images/joaobairradaResume.pdf"
       }
     },
-    aboutMePhoto: '/../assets/images/bairrada.png',
+    aboutMePhoto: "/../assets/images/bairrada.png",
     about: {
-
       language: {
         id: 0,
-        image: '/../assets/images/language.png',
+        image: "/../assets/images/language.png",
         text: "Portuguese and English"
       },
       mouse: {
         id: 1,
-        image: '/../assets/images/rato.png',
+        image: "/../assets/images/rato.png",
         text: "Currently working at Fullsix"
       },
       photo: {
         id: 2,
-        image: '/../assets/images/photo.png',
+        image: "/../assets/images/photo.png",
         text: "Photography and Coding"
       },
       keyboard: {
         id: 3,
-        image: '/../assets/images/teclado.png',
+        image: "/../assets/images/teclado.png",
         text: "Skills: HTML, Css/Sass, Javascript/Jquery, Vue.js, Git and Gulp"
       },
       roots: {
         id: 4,
-        image: '/../assets/images/raizes.png',
+        image: "/../assets/images/raizes.png",
         text: "Lisbon, Portugal"
       },
       diploma: {
         id: 5,
-        image: '/../assets/images/canudo.png',
+        image: "/../assets/images/canudo.png",
         text: "Course in Front-End"
       },
       brain: {
         id: 6,
-        image: '/../assets/images/brain.png',
+        image: "/../assets/images/brain.png",
         text: "I'm a Front-End Developer"
       }
-
     },
     projectsToShow: 3,
     interestsToShow: 3,
     transition: false,
     changePerspective: false,
-    perspectiveText: '3D Mode',
+    perspectiveText: "3D Mode",
     isMobile: false,
     rangeSliderValue: null,
     maxRangeSliderValue: 6,
@@ -210,14 +214,13 @@ export default new Vuex.Store({
   getters: {
     showMoreProjects: state => state.projectsToShow + 3,
     showMoreInterests: state => state.interestsToShow + 3,
-    showModal: (state) => {
+    showModal: state => {
       for (let i = 0; i < state.interests.length; i++) {
         if (state.interests[i].isSelected) return state.interests[i];
-
       }
-
     },
-    getProject: state => id => state.projects.find(project => project.id === id),
+    getProject: state => id =>
+      state.projects.find(project => project.id === id),
     routeTransition: state => state.transition,
     changePerspective: state => state.changePerspective,
     getRangeSliderValue: state => state.rangeSliderValue,
@@ -233,7 +236,7 @@ export default new Vuex.Store({
       state.interestsToShow = payload;
     },
     isVisible: (state, payload) => {
-      state.interests.forEach((element) => {
+      state.interests.forEach(element => {
         element.isSelected = false;
       });
       state.interests[payload].isSelected = true;
